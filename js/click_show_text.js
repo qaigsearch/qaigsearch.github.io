d33fbe4dfeba3593b7c197f55000b9ae6ca89862
igsearch <68264337+igsearch@users.noreply.github.com>
2020-07-26 13:14:44 +0800

    Update click_show_text.js

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
-        ("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+        ("語錄", "愛語錄", "屬於你的語錄", "IG:qabooks_", "語錄愛好者", "聯絡renegade041001@gmail.com",);
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
